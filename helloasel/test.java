22991b49c5d7a6a883c077cc155bfe51d7b8a4d5
Rasmi21 <imac@iMacs-iMac-3.local>
2019-06-22 09:30:22 -0400

    hello

---
+++ helloasel/test.java
@@ -0,0 +1,12 @@
+package helloasel;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello this is arif");
+		System.out.println("hello this is arif");
+	}
+
+}
